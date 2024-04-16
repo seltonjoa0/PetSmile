457bba1a73d344c90c182f91cdb9168b8cd78c1d
Selton Duarte <162406541+seltonjoa0@users.noreply.github.com>
2024-04-16 23:20:24 +0000

    entrega_18

---
+++ entrega_18.js
@@ -0,0 +1,114 @@
+console.log("------------------------------------------");
+console.log("|            SISTEMA PETSMILE            |");
+console.log("|----------------------------------------|");
+console.log("|\t\t\t\t\t |");
+console.log("|\t\t Usuário:   \t\t |");
+console.log("|\t\t Senha:   \t\t |");
+console.log("|\t\t\t\t\t |");
+console.log("|________________________________________|");
+
+
+console.log("------------------------------------------");
+console.log("|            SISTEMA PETSMILE            |");
+console.log("|----------------------------------------|");
+console.log("|\t\t\t\t\t |");
+console.log("|\t    (1) Vendas   \t\t |");
+console.log("|\t    (2) Estoque   \t\t |");
+console.log("|\t    (3) Sair do sistema   \t |");
+console.log("|\t\t\t\t\t |");
+console.log("|________________________________________|");
+
+
+console.log("------------------------------------------");
+console.log("|                 VENDAS                 |");
+console.log("|----------------------------------------|");
+console.log("|\t\t\t\t\t |");
+console.log("|\t    (1) Vendas Pendentes  \t |");
+console.log("|\t    (2) Vendas Realizadas   \t |");
+console.log("|\t    (3) Vendas Canceladas  \t |");
+console.log("|\t    (4) Voltar  \t\t |");
+console.log("|\t\t\t\t\t |");
+console.log("|________________________________________|");
+
+
+console.log("------------------------------------------");
+console.log("|            VENDAS PENDENTES            |");
+console.log("|----------------------------------------|");
+console.log("|\t Opção | ID Venda | Valor \t |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t  (1)\t   #07 \t    R$ 350,00    |");
+console.log("|\t  (2)\t   #08 \t    R$ 115,00    |");
+console.log("|\t  (3)\t   #09 \t    R$ 20,00     |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t  (0) Para Voltar \t\t |");
+console.log("|________________________________________|");
+
+
+console.log("------------------------------------------");
+console.log("|            VENDAS PENDENTES            |");
+console.log("|----------------------------------------|");
+console.log("|\t\t\t\t\t |");
+console.log("|\t (1) Para confirmar a venda\t |");
+console.log("|\t (2) Para cancelar a venda\t |");
+console.log("|\t (3) Para voltar \t\t |");
+console.log("|\t\t\t\t\t |");
+console.log("|________________________________________|"); 
+
+
+console.log("------------------------------------------");
+console.log("|            VENDAS REALIZADAS           |");
+console.log("|----------------------------------------|");
+console.log("|\t Opção | ID Venda | Valor \t |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t  (1)\t   #01 \t     350,00  \t |");
+console.log("|\t  (2)\t   #02 \t     115,00  \t |");
+console.log("|\t  (3)\t   #03 \t     20,00   \t |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t  (0) Para Voltar \t\t |");
+console.log("|________________________________________|"); 
+
+console.log("------------------------------------------");
+console.log("|            VENDAS REALIZADAS           |");
+console.log("|----------------------------------------|");
+console.log("|    Cliente  |   Valor   |  ID Venda    |");
+console.log("|\t\t\t\t\t |");
+console.log("|     Ana        350,00         #1       |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t\t\t\t\t |");
+console.log("|    (0) Para Voltar \t\t\t |");
+console.log("|________________________________________|"); 
+
+console.log("------------------------------------------");
+console.log("|            VENDAS CANCELADAS           |");
+console.log("|----------------------------------------|");
+console.log("|\t Opção | ID Venda | Valor \t |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t  (1)\t   #04 \t    531,35 \t |");
+console.log("|\t  (2)\t   #05 \t    67,21  \t |");
+console.log("|\t  (3)\t   #06 \t    78,00  \t |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t  (0) Para Voltar \t\t |");
+console.log("|________________________________________|") 
+
+console.log("------------------------------------------");
+console.log("|            VENDAS CANCELADAS           |");
+console.log("|----------------------------------------|");
+console.log("|    Cliente  |   Valor   |  ID Venda    |");
+console.log("|\t\t\t\t\t |");
+console.log("|     Ítalo       531,35       #4        |");
+console.log("|\t\t\t\t\t |");
+console.log("|\t\t\t\t\t |");
+console.log("|    (0) Para Voltar \t\t\t |");
+console.log("|________________________________________|") 
+
+console.log("------------------------------------------");
+console.log("|                 ESTOQUE                |");
+console.log("|----------------------------------------|");
+console.log("|\t\t\t\t\t |");
+console.log("|\t    (1) Exibir Produtos  \t |");
+console.log("|\t    (2) Editar Produtos   \t |");
+console.log("|\t    (3) Criar Produtos  \t |");
+console.log("|\t    (4) Exluir Produtos \t |");
+console.log("|\t    (0) Voltar \t\t\t |");
+console.log("|\t\t\t\t\t |");
+console.log("|________________________________________|");
